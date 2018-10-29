0178f2fd64c621de03bcffbe64dd4f6ae8977f73
Garron Ma <garronma@gmail.com>
2018-10-29 13:46:46 -0700

    config files

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+/**
+ * ************************************
+ *
+ * @module  Index.js
+ * @author Team Quail
+ * @date
+ * @description 
+ *
+ * ************************************
+ */
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
+
+ReactDOM.render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById("App")
+);
