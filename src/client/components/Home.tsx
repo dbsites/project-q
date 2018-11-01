ea22e89127b6128664f6b65fb0ef5950be589ad5
Garron Ma <garronma@gmail.com>
2018-11-01 16:46:08 -0700

    add placeholder home component for '/'

---
+++ src/client/components/Home.tsx
@@ -0,0 +1,23 @@
+/**
+ * @module Home.tsx
+ * @description Home Page Component
+ */
+
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home: any = () => (
+  <div>
+    <h1>Welcome to EthiQ</h1>
+    <ul>
+      <li>
+        <Link to='/account/login'>Log In</Link>
+      </li>
+      <li>
+        <Link to='/account/register'>Register</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Home;
