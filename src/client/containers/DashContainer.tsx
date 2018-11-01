91f14363604b682bd08f4ae22f063cabea7b2966
Garron Ma <garronma@gmail.com>
2018-11-01 16:46:34 -0700

    create Dash container (not connected)

---
+++ src/client/containers/DashContainer.tsx
@@ -0,0 +1,14 @@
+/**
+ * @module DashContainer.tsx
+ * @description Dashboard Container for User 
+ */
+
+import * as React from 'react';
+
+const DashContainer: any = () => (
+  <div>
+    <h1>User Dashboard</h1>
+  </div>
+);
+
+export default DashContainer;
