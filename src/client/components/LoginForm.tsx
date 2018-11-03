28385f77d9b3c4617399d8693fadeb14f8e7e67a
Serge Vartanov <vartanov.s@gmail.com>
2018-11-02 20:29:00 -0700

    Added Styling and Disclaimer

--- src/client/components/LoginForm.tsx
+++ src/client/components/LoginForm.tsx
@@ -4,6 +4,7 @@
  */
 
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import FormField from './FormField';
 // import FormError from './FormError';
 
@@ -23,14 +24,16 @@ const LoginForm: any = (props: any) => {
   } = loginFields;
 
   return (
-    <div>
-      <FormField field={loginEmail} name="loginEmail" type="text" updateField={updateLoginField} >Email: </FormField>
+    <div className="login-register-form">
+      <h2>Sign in to your account</h2> 
+      <div className="change-form-link">Don't have an account? <Link to='/account/register'>Sign up</Link></div>
+      <FormField field={loginEmail} name="loginEmail" type="text" updateField={updateLoginField} >Email </FormField>
       <br />
-      <FormField field={loginPassword} name="loginPassword" type="password" updateField={updateLoginField} >Password: </FormField>
+      <FormField field={loginPassword} name="loginPassword" type="password" updateField={updateLoginField} >Password </FormField>
       <br />
-      <FormField field={rememberMe} name="rememberMe" type="checkbox" updateField={updateLoginField} >Remember Me: </FormField>
+      <FormField field={rememberMe} name="rememberMe" type="checkbox" updateField={updateLoginField} >Remember Me </FormField>
       <br />
-      <input onClick={() => fetchLoginRequest(loginFields)} type="submit"/>
+      <input className="submit-button" onClick={() => fetchLoginRequest(loginFields)} type="submit"/>
     </div>
   )
 }
