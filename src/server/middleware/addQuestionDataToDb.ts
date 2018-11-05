2b909db4ce51258cacc2d8b43f214992d7cc38e7
Emeka <emekaokonkwo@Emekas-MacBook-Pro.local>
2018-11-05 12:50:37 -0800

    Created an endpoint to grab questions from DB

--- src/server/middleware/addQuestionDataToDb.ts
+++ src/server/middleware/addQuestionDataToDb.ts
@@ -11,7 +11,7 @@
 
 import { Request, Response, NextFunction } from 'express';
 // import db and db functionality from modular files to access db methods
-import db from '../../db';
+import db from '../index'
 
 const QuestionDatabase: any = {};
 
@@ -25,6 +25,18 @@ QuestionDatabase.insertData = (req: Request, _: Response, next: NextFunction) =>
     console.log('ERROR AT INSERT DATA IN addQuestionToDb', error);
   });
   
+}
+
+QuestionDatabase.getQuestionList = (_: Request, res: Response, next: NextFunction) => {
+  db.questions.getQuestions()
+    .then((data: any[]) => {
+      res.locals.questionDataArray = data;
+      next();
+    })
+    .catch((error: any) => {
+      console.log('ERROR AT getQuestionList IN addQuestionsDataToDb.ts', error);
+      res.sendStatus(500);
+    })
 }
 
 export default QuestionDatabase;
