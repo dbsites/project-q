1ce0dbaefd5089b3f1a4a23dc7114c4e70021c7d
Garron Ma <garronma@gmail.com>
2018-11-06 16:08:25 -0800

    Presentational container for all four quads

---
+++ src/client/components/QuadsDisplay.tsx
@@ -0,0 +1,27 @@
+/**
+ * @module QuadsDisplay.tsx
+ * @description Quads display component
+ */
+
+import * as React from 'react';
+
+import Overview from '../components/Overview';
+import StockGraph from '../components/StockGraph';
+import IssuesCharts from '../components/IssuesCharts';
+import CompanyList from '../components/CompanyList';
+
+const QuadsDisplay = (props: any) => {
+  return (
+    <React.Fragment>
+      <Overview selected={props.selected} />
+      <StockGraph selected={props.selected} />
+      <IssuesCharts selected={props.selected} />
+      <CompanyList
+        list={props.list}
+        sortListBy={props.sort}
+        selectCompany={props.select}
+      />
+    </React.Fragment>
+  )
+}
+export default QuadsDisplay;
