13f318514ed8b72e5129ee0d9efc89af3e6615d9
Garron Ma <garronma@gmail.com>
2018-11-12 13:20:58 -0800

    added TS interface to props

--- src/client/components/Overview.tsx
+++ src/client/components/Overview.tsx
@@ -5,35 +5,71 @@
 
 import * as React from 'react';
 
-const Overview = (props: any) => {
+import { PieChart } from 'react-easy-chart';
 
-  // console.log('in overview:', props.selected)
+interface CompanyInfo {
+  readonly description: string,
+  readonly logo: string,
+  readonly ticker: string,
+  readonly name: string,
+  readonly overallScore: number,
+}
 
+interface SelectedCompany {
+  readonly selected: CompanyInfo
+}
+
+const Overview = (props: SelectedCompany) => {
+
+  console.log('overview in props: ', props);
   let display;
 
+  //TODO: styling --> center justify this to quad
   if (!props.selected.ticker) {
     display = (<p>Click a company to see their overview</p>);
   }
 
   else {
-    const { description, logo, ticker, name } = props.selected;
+    const { logo, description, ticker/*, overallScore , name*/ } = props.selected;
+
+    const overall = Math.floor(Math.random() * 100);
+    const overallColor = overall >= 50 ? '#005005' : '#8e0000';
+    const diff = 100 - overall;
+    console.log('overall: ', overall, ' diff: ', diff);
+
+
     display = (
-      <ul id="list-overview">
-        <li key={ticker}>
-          <h2>{name} ({ticker})</h2>
-          <img src={logo} id="overview-company-logo" />
+      <React.Fragment>
+        <div id="overview-left">
+          <h3>Overall Score</h3>
+          <h1>{overall}</h1>
+          <div id="overall-score-chart">
+            <PieChart
+              size={150}
+              data={[
+                { key: 'Overall', value: overall/*overallScore*/, color: overallColor },
+                { key: 'Difference', value: diff, color: '#000000' }
+              ]}
+            />
+          </div>
+          <p>Market Price: {ticker}</p>
+          <p>Founded: 1975</p>
+          <p># of Employees: 175</p>
+          <a href="#">Website</a>
+        </div>
+        <div id="overview-right">
+          <img src={logo} id="overview-logo" />
           <p>{description}</p>
-        </li>
-      </ul>
+        </div>
+
+      </React.Fragment>
     )
   }
   return (
     <div className='quad' id="quad-overview">
-      <div className="quad-container">
+      <div className="quad-container-overview">
         {display}
-
       </div>
-      {/* <h3>Overview</h3> */}
     </div>
   );
 }
