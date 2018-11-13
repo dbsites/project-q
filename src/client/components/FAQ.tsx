39b9905d566c457473c723a6408186005ff58d47
Serge Vartanov <vartanov.s@gmail.com>
2018-11-12 22:06:01 -0800

    Initial Update to FAQ

--- src/client/components/FAQ.tsx
+++ src/client/components/FAQ.tsx
@@ -5,27 +5,36 @@
 
 import * as React from 'react';
 
+import Header from '../containers/HeaderContainer';
+
 const FAQ = () => {
   return (
-    <div>
-      <div>
-        FAQ - What is Ethiq?
+    <div className="main-dashboard">
+      <div className="header">
+        <Header />
       </div>
-      <p>
-        Ethiq® is a tool designed to help you make purchases more aligned with your beliefs and values; to Vote With Your Wallet®. Our core mission is transparency through objective, factual data.
-      </p>
-      <p>
-        How does Ethiq match me with compatible companies?
-      </p>
-      <p>
-        An Ethiq user submits their opinion on a variety of prompts relating to ideological positions, on-the-record positions and statement by politicians, specific bills, ballot measures, etc. As you do this, Ethiq’s propriety algorithm tracks the sponsors, authors and votes related to these prompts, cross-referencing your responses with a dataset that links corporate money and its paths to these individuals and groups, both directly and indirectly. Currently, Ethiq offers customized rankings in 4 key consumer categories: Gas Stations, Grocery Stores, Department Stores, and Fast Food Restaurants. Please let us know if there which additional categories you’d like us to add.
-      </p>
-      <p>
-        Where does Ethiq lean on the political spectrum – across political positions, ideologies and party lines?
-      </p>
-      <p>
-        Ethiq is a tool designed for Republicans, Democrats, Liberals, Conservatives, and everything in between. There are values-driven consumers on all sides of the political spectrum. The results Ethiq outputs, depending on your unique responses, should be equally valid to every user. Try for yourself! Click “Start Over” in the prompt box, and answer the prompts as your ideological opposite. You’ll soon see how the results match the “new you.”
-      </p>
+      <div className="dashboard-header dashboard-issue-header">
+        <span>
+          FAQ - What is Ethiq?
+        </span>
+      </div>
+      <div className="issue-dashboard-container">
+        <p>
+          Ethiq® is a tool designed to help you make purchases more aligned with your beliefs and values; to Vote With Your Wallet®. Our core mission is transparency through objective, factual data.
+        </p>
+        <p>
+          How does Ethiq match me with compatible companies?
+        </p>
+        <p>
+          An Ethiq user submits their opinion on a variety of prompts relating to ideological positions, on-the-record positions and statement by politicians, specific bills, ballot measures, etc. As you do this, Ethiq’s propriety algorithm tracks the sponsors, authors and votes related to these prompts, cross-referencing your responses with a dataset that links corporate money and its paths to these individuals and groups, both directly and indirectly. Currently, Ethiq offers customized rankings in 4 key consumer categories: Gas Stations, Grocery Stores, Department Stores, and Fast Food Restaurants. Please let us know if there which additional categories you’d like us to add.
+        </p>
+        <p>
+          Where does Ethiq lean on the political spectrum – across political positions, ideologies and party lines?
+        </p>
+        <p>
+          Ethiq is a tool designed for Republicans, Democrats, Liberals, Conservatives, and everything in between. There are values-driven consumers on all sides of the political spectrum. The results Ethiq outputs, depending on your unique responses, should be equally valid to every user. Try for yourself! Click “Start Over” in the prompt box, and answer the prompts as your ideological opposite. You’ll soon see how the results match the “new you.”
+        </p>
+      </div>
     </div>
   )
 };
