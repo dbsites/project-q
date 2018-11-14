2554f407c4a1ecc2922646a74eca91ba595e8455
joelkperkins <joel.climbs@gmail.com>
2018-11-13 18:16:15 -0800

    added issue initials to the company list response object

--- src/server/daemonServer.ts
+++ src/server/daemonServer.ts
@@ -19,8 +19,13 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 // import helmet to increase app security
 import * as helmet from 'helmet';
+// import node-fetch to hit the finance api
+import fetch from 'node-fetch';
+// import the env files
+import * as dotenv from 'dotenv';
+dotenv.config();
 // import companyDb middleware
-// import CompanyDatabase from './middleware/companyDataMethods';
+import CompanyDatabase from './middleware/companyDataMethods';
 // import database middleware
 // import DatabaseMethods from './middleware/additionalDataMethods';
 
@@ -32,7 +37,7 @@ const app: Application = express();
 const PORT = 6000;
 
 // define interval constant
-// const interval = 60 * 1000;
+const fiveSeconds = 5000;
 
 // Allow CORS, credentials true expects  request to come with credentials and origin specifies where they should come from
 app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
@@ -49,18 +54,50 @@ app.use(bodyParser.json({limit: '10mb'}));
 // one call per stock ticker
 // -> query for tickers 
 
-// async function getStockPrices() {
-//   const stockSymbols: any = [];
-//   await CompanyDatabase.getTickers();
-// }
+function getStockPrices() {
 
-// function showSymbols() {
-//   return 
-// }
+  CompanyDatabase.getTickers()
+  .then((stockSymbols: any[]) => {
 
-// stockSymbols.forEach((item: any) => {
-//   console.log(item);
-// });
+    let startSymbol = 0;
+    let stockInterval = setInterval(() => { getStockData() }, fiveSeconds)
+
+    if (startSymbol === 500) {
+      clearInterval(stockInterval);
+    }
+    
+    async function getStockData() {
+      for (let currSymbol = startSymbol; currSymbol < 1; currSymbol += 1) {
+        // make the api call
+        // set the date to a year ago
+        let oneYearAgo: any = Array.from(JSON.stringify(new Date()).substring(1,11)).map((item: any, index: any) => {
+          if (index === 3) {
+            return '7'
+          }
+          return item;
+        });
+        
+        await fetch(`https://api.intrinio.com/prices?identifier=${stockSymbols[currSymbol].ticker.split(".")[0]}&start_date=${oneYearAgo.join("")}&frequency=monthly&api_key=${<string>process.env.STOCK_API_KEY}`)
+        .then((response: any) => response.json())
+        .then((response: any) => {
+          console.log(response.data[0]);
+          CompanyDatabase.storeRecentStockData(response.data[0], stockSymbols[currSymbol].ticker);
+        })
+        .catch((err: any) => console.error(err));
+        // store the data
+        // update the currSymbol
+      }
+    }
+    return true;
+  })
+  .catch(() => {
+    
+  })
+}
+
+console.log(JSON.stringify(new Date()).substring(1,11));
+getStockPrices();
+
 
 
 // iterate through the tickers 
