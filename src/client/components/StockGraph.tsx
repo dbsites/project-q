ceeb7a86d44017559e45089548497e2d2cf2f038
Garron Ma <garronma@gmail.com>
2018-11-16 15:05:17 -0800

    change default wording

--- src/client/components/StockGraph.tsx
+++ src/client/components/StockGraph.tsx
@@ -10,7 +10,7 @@ const StockGraph = (props: any) => {
   let display: any;
 
   if (!props.selected) {
-    display = (<p>Click company for stock market performance</p>)
+    display = (<p>Select a company for stock market performance</p>)
   } else {
     const { ticker } = props.selected;
     const URI = `https://api.stockdio.com/visualization/financial/charts/v1/ComparePrices?app-key=50EC4535F41E4734BC8AD78686377BAC&symbol=${ticker.split('.')[0]}&indices=SPX&includeCompetitors=true&palette=Relief&showLogo=No&animate=true&googleFont=true&backgroundColor=000000`;
