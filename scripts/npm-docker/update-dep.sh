228540f2b9f7cec4dcfdb9e509713043746abbde
smozingo <schno.mozingo@gmail.com>
2018-11-16 14:24:51 -0800

    Added docker, aws, db-migrate

---
+++ scripts/npm-docker/update-dep.sh
@@ -0,0 +1,10 @@
+# 1. Removes image from local environment
+# 2. Rebuilds image with updated package.json
+
+echo -e "\033[1;33m'CTRL + Z' to kill this script\033[0m"
+
+echo -e "\033[1;36mRemoving existing image from local environment...\033[0m"
+docker image rm dbsites/ethiq-dependencies --force
+echo -e "\033[1;36mRebuilding image with updated package.json...\033[0m"
+docker build -t dbsites/ethiq-dependencies -f Dockerfile-dev .
+echo -e "\033[1;32mDone!\033[0m"
