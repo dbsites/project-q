abe45460bca0d0b3c1876619aeb9e8083e094b41
Serge Vartanov <vartanov.s@gmail.com>
2018-11-16 18:44:53 -0800

    Docker DevOps Configuration and Additional Testing

---
+++ webpack/prod.config.js
@@ -0,0 +1,78 @@
+/**
+ * ************************************
+ *
+ * @module  webpack.config.js
+ * @author Team Quail
+ * @description Webpack Configuration
+ *
+ * ************************************
+ */
+
+const HTMLWebpack = require('html-webpack-plugin');
+const path = require('path');
+
+const HTMLWebPackPlugin = new HTMLWebpack({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(`${__dirname}/dist`),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'awesome-typescript-loader',
+        },
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'source-map-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  plugins: [HTMLWebPackPlugin],
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
+};
