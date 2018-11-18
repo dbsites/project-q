9159f958526ce1ea6f276cdca8305cc3ed4628e5
joelkperkins <joel.climbs@gmail.com>
2018-11-17 17:03:52 -0800

    api testing written for primary application routes

--- src/server/controllers/data.ts
+++ src/server/controllers/data.ts
@@ -85,7 +85,7 @@ export class DatabaseRepository {
 
   async insertPoliticianData(dataObject: any) {
     for (let i = 0; i < dataObject.length; i += 1) {
-      await this.db.none('INSERT INTO politicians (id, company_id, recip_1, recip_1_amount, recip_1_img, recip_2, recip_2_amount, recip_2_img, recip_3, recip_3_amount, recip_3_img) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);', [v4(), dataObject[i].company_id, dataObject[i].recipient_one, dataObject[i].recipient_one_amount, dataObject[i].recipient_one_image, dataObject[i].recipient_two, dataObject[i].recipient_two_amount, dataObject[i].recipient_two_image, dataObject[i].recipient_three, dataObject[i].recipient_three_amount, dataObject[i].recipient_three_image]);
+      await this.db.none('INSERT INTO politicians (id, company_id, aggregate_amount, recip_1, recip_1_amount, recip_1_img, recip_2, recip_2_amount, recip_2_img, recip_3, recip_3_amount, recip_3_img) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);', [v4(), dataObject[i].company_id, dataObject[i].aggregate_amount, dataObject[i].recipient_one, dataObject[i].recipient_one_amount, dataObject[i].recipient_one_image, dataObject[i].recipient_two, dataObject[i].recipient_two_amount, dataObject[i].recipient_two_image, dataObject[i].recipient_three, dataObject[i].recipient_three_amount, dataObject[i].recipient_three_image]);
     }
   }
 
