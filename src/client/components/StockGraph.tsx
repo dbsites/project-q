55b108d4b7f9d849ef5aba9de079c3daba18e919
Garron Ma <garronma@gmail.com>
2018-11-17 16:54:54 -0800

    render company stock info bar

--- src/client/components/StockGraph.tsx
+++ src/client/components/StockGraph.tsx
@@ -13,6 +13,18 @@ const StockGraph = (props: any) => {
     display = (<p>Select a company for stock market performance</p>)
   } else {
     const { ticker } = props.selected;
+    let high, low, open, close, volume;
+
+    if (!props.selectedData.stockData) {
+      high = 0; low = 0; open = 0; close = 0; volume = 0;
+    } else {
+      high = props.selectedData.stockData.high;
+      low = props.selectedData.stockData.low;
+      open = props.selectedData.stockData.open;
+      close = props.selectedData.stockData.close;
+      volume = props.selectedData.stockData.volume;
+    }
+
     const URI = `https://api.stockdio.com/visualization/financial/charts/v1/ComparePrices?app-key=50EC4535F41E4734BC8AD78686377BAC&symbol=${ticker.split('.')[0]}&indices=SPX&includeCompetitors=true&palette=Relief&showLogo=No&animate=true&googleFont=true&backgroundColor=000000`;
 
     display = (
@@ -21,11 +33,11 @@ const StockGraph = (props: any) => {
         </iframe >
         <div className="stock-container-info">
           <ul>
-            <li>High: </li>
-            <li>Low: </li>
-            <li>Open: </li>
-            <li>Close: </li>
-            <li>Volume: </li>
+            <li>High: {high}</li>
+            <li>Low: {low}</li>
+            <li>Open: {open}</li>
+            <li>Close: {close}</li>
+            <li>Volume: {volume}</li>
           </ul>
         </div>
       </React.Fragment>
