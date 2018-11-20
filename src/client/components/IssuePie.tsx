7e4f950b7da56ac417d5864af027153491eae16e
Garron Ma <garronma@gmail.com>
2018-11-19 20:26:12 -0800

    add blurb

--- src/client/components/IssuePie.tsx
+++ src/client/components/IssuePie.tsx
@@ -7,22 +7,103 @@ import * as React from 'react';
 import { Component } from 'react';
 import { PieChart, Pie, Cell, Label, ResponsiveContainer } from 'recharts';
 
+import IssueScripts from '../IssueScripts';
 
 interface Props {
   info: any
   modal?: any
-  politician?: any
+  polit?: any
 }
 
 class IssuePie extends Component<Props> {
   constructor(props: any) {
     super(props);
   }
-  render() {
 
+  render() {
     const { name, alignedScore } = this.props.info;
-    console.log('aligned score: ', alignedScore);
-    let display;
+    let blurb, display;
+
+    if (this.props.modal) {
+
+      const {
+        aggregate_amount,
+        recip_1,
+        recip_1_amount,
+        recip_2,
+        recip_2_amount,
+        recip_3,
+        recip_3_amount
+      } = this.props.polit;
+
+      const {
+        green_buildings,
+        targets_emissions,
+        human_rights,
+        policy_board_diversity,
+        women_managers,
+        nra_score,
+        brady_rating,
+        tsr,
+        salary_gap,
+        community_score,
+        charity_amount,
+        yes_manchin,
+        no_manchin,
+        yes_repeal,
+        no_repeal,
+        yes_tax_cut,
+        no_tax_cut,
+        diversity_score,
+        aila_score,
+        fair_score,
+        taxes_paid,
+        trump_alignment,
+        norml_score,
+        company_name,
+      } = this.props.modal;
+
+      const searchName = name.split(' ')[0].toUpperCase();
+
+      switch (searchName) {
+        case "MONEY":
+          blurb = IssueScripts[name](company_name, alignedScore, name, aggregate_amount, recip_1, recip_1_amount, recip_2, recip_2_amount, recip_3, recip_3_amount);
+          break;
+        case "ENVIRONMENT":
+          blurb = IssueScripts[name](company_name, alignedScore, name, green_buildings, targets_emissions);
+          break
+        case "CIVIL/WOMEN'S":
+          blurb = IssueScripts[name](company_name, alignedScore, name, human_rights, policy_board_diversity, women_managers);
+          break;
+        case "2ND":
+          blurb = IssueScripts[name](company_name, alignedScore, name, nra_score, brady_rating, yes_manchin, no_manchin);
+          break;
+        case "EXECUTIVE":
+          blurb = IssueScripts[name](company_name, alignedScore, name, tsr, salary_gap);
+          break;
+        case "CORPORATE":
+          blurb = IssueScripts[name](company_name, alignedScore, name, community_score, charity_amount);
+          break;
+        case "DRUG":
+          blurb = IssueScripts[name](company_name, alignedScore, name, norml_score);
+          break;
+        case "HEALTH":
+          blurb = IssueScripts[name](company_name, alignedScore, name, yes_repeal, no_repeal);
+          break;
+        case "IMMIGRATION":
+          blurb = IssueScripts[name](company_name, alignedScore, name, diversity_score, aila_score, fair_score);
+          break;
+        case "TAXES":
+          blurb = IssueScripts[name](company_name, alignedScore, name, yes_tax_cut, no_tax_cut);
+          break;
+        case "PRESIDENTIAL":
+          blurb = IssueScripts[name](company_name, alignedScore, name, trump_alignment);
+          break;
+        case "ECONOMY":
+          blurb = IssueScripts[name](company_name, alignedScore, name, taxes_paid);
+          break;
+      }
+    }
 
     if (!alignedScore) {
       display = (
@@ -57,6 +138,8 @@ class IssuePie extends Component<Props> {
           value: alignedScore
         }
       ];
+
+      console.log('blurb = ', blurb);
 
       display = (
         <ResponsiveContainer>
