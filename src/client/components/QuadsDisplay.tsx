263847eac1686b36cccac6ed033daf88be7bc63f
Garron Ma <garronma@gmail.com>
2019-01-30 15:25:22 -0800

    remove pie index prop

--- src/client/components/QuadsDisplay.tsx
+++ src/client/components/QuadsDisplay.tsx
@@ -18,7 +18,6 @@ const QuadsDisplay = (props: any) => {
       <StockGraph selected={props.selected} stockData={props.stockData} />
       <IssuesCharts
         displayDetail={props.displayDetails}
-        // pieIndex={props.pieHoverIndex}
         hoverOn={props.hoverOn}
         hoverOff={props.hoverOff}
         selectedCompany={props.selected}
