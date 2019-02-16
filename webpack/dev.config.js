a9e17e93a3a9630952bc4805c2d225c15b9e778a
Garron Ma <garronma@gmail.com>
2019-02-15 20:23:17 -0800

    change proxy api to localhost

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -37,7 +37,7 @@ module.exports = merge(common, {
     historyApiFallback: true,                             // Redirect 404s back to /index.html
     host: '0.0.0.0',                                      // Bind host for Docker compatibility
     proxy: {
-      '/api': 'http://ethiq-dev-server:3000',             // Proxy requests to '8080/api' route
+      '/api': 'http://localhost:3000',                    // Proxy requests to '8080/api' route
     },
     port: 8080,                                           // Specify PORT for requests
   },
