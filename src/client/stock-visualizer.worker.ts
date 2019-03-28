97d23ec14300ef457b49a0ed7485a5ae1a1bbc9e
Volodymyr Bunin <volodymyr@xdesign.ws>
2019-03-28 12:31:32 +0200

    filter by date lofic

--- src/client/stock-visualizer.worker.ts
+++ src/client/stock-visualizer.worker.ts
@@ -6,35 +6,51 @@ const sendMessage: any = self.postMessage;
 const XLSX_FILE_URL = './HistoricalDataFull.xlsx';
 
 onmessage = (e) => {
+
+  const _getMaxDate = (filteredRaw: any) => {
+    const moments: any = filteredRaw
+      .map((el: any) => el.lol.map((ell: any) => ell.date))
+      .map((el: any) => new Date(el[1]));
+
+    return new Date(Math.max.apply(null, moments));
+  }
+
   const { event, stocksList /*data,*/} = e.data;
   switch (event){
     case 'readData':
 
       /* set up async GET request */
-      var req = new XMLHttpRequest();
+      let req = new XMLHttpRequest();
       req.open("GET", XLSX_FILE_URL, true);
       req.responseType = "arraybuffer";
 
-      req.onload = (_) => {
+      req.onload = () => {
         const data = new Uint8Array(req.response);
         const workbook = XLSX.read(data, {type:"array"});
-        // var workbook = XLSX.read(data, {type: 'binary'});
-        const startDate = '01-01-2018';
-        const endDate = '03-01-2019';
-        const sp500 = XLSX.utils
-          .sheet_to_json(workbook.Sheets.Sheet1)
+        // const workbook = XLSX.read(data, {type: 'binary'});
+        // const startDate = '01-01-2015';
+        // const endDate = '03-01-2019';
+        const raw = XLSX.utils.sheet_to_json(workbook.Sheets.Sheet1);
+        // console.log(raw, 'raw');
+        const raw2 = raw
           .map((ugly: any) => ({
             name: ugly.__EMPTY,
             lol: Object
               .keys(ugly)
               .map(k => ({date: k, value: ugly[k]}))
-          }))
-          .filter((el: any) => ~(stocksList.indexOf(el.name)))
+          }));
+        // console.log(raw2, 'raw2');
+        const filteredRaw = raw2.filter((el: any) => ~(stocksList.indexOf(el.name)));
+
+        const startDate = _getMaxDate(filteredRaw);
+        // console.log(startDate, 'startDate');
+
+        const sp500 = filteredRaw
           .map((el: any) => ({
             ...el,
             lol: el.lol
-              .filter((ell: any) => moment(ell.date).isSameOrAfter(startDate)
-                && moment(ell.date).isSameOrBefore(endDate))
+              .filter((ell: any) => moment(ell.date).isSameOrAfter(startDate))
+                // && moment(ell.date).isSameOrBefore(endDate))
           }));
 
         sendMessage({event: 'SUCCESS', data: sp500});
