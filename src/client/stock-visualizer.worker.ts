4324e3718c0d7c8ae0cec6a9e30d3a1d318a9ba9
Volodymyr Bunin <volodymyr@xdesign.ws>
2019-04-03 18:40:51 +0300

    error if not loaded file

--- src/client/stock-visualizer.worker.ts
+++ src/client/stock-visualizer.worker.ts
@@ -2,9 +2,7 @@ import * as XLSX from 'xlsx';
 import * as moment from 'moment';
 
 const sendMessage: any = self.postMessage;
-// const XLSX_FILE_URL = './HistoricalDataTest.xlsx';
-// const XLSX_FILE_URL = './HistoricalDataFull.xlsx';
-const XLSX_FILE_URL = './UpdatedHistoricalDataFull4.xlsx';
+const XLSX_FILE_URL = '/UpdatedHistoricalDataFull4.xlsx';
 
 onmessage = (e) => {
 
@@ -25,18 +23,22 @@ onmessage = (e) => {
   const { event, stocksList, /*topStocksFilter data,*/} = e.data;
   switch (event){
     case 'readData':
-      /* set up async GET request */
       let req = new XMLHttpRequest();
       req.open("GET", XLSX_FILE_URL, true);
       req.responseType = "arraybuffer";
 
       req.onload = () => {
+        if(req.status !== 200) {
+          return sendMessage({
+            event: 'ERROR',
+            data: `Error. ${req && req.statusText}`
+          });
+        }
+
         const data = new Uint8Array(req.response);
         const workbook = XLSX.read(data, {type:"array"});
-        // const startDate = '01-01-2015';
-        // const endDate = '03-01-2019';
         const raw = XLSX.utils.sheet_to_json(workbook.Sheets.Sheet1);
-        // console.log(raw, 'raw');
+
         const raw2 = raw.map((ugly: any) => ({
           fullName: ugly['Company Names'],
           name: ugly.__EMPTY,
@@ -44,9 +46,9 @@ onmessage = (e) => {
             .keys(ugly)
             .map(k => ({date: k, value: ugly[k]}))
         }));
-        // console.log(raw2, 'raw2');
+
         const filteredRaw = raw2.filter((el: any) => ~(stocksList.indexOf(el.name)));
-        console.log(filteredRaw.map(el => el.name), 'filteredRaw');
+        // console.log(filteredRaw.map(el => el.name), 'filteredRaw');
 
         const rawLengthWithoutSp500 = (filteredRaw.length - 1);
 
@@ -85,7 +87,6 @@ onmessage = (e) => {
           event: 'SUCCESS',
           data: sp500,
           companiesCount: rawLengthWithoutSp500,
-          // startDateCompanyName: startDateCompany && startDateCompany.n
           startDateCompanyName: startDateCompany
             && `${startDateCompany.fullName} (${startDateCompany.n})`
         });
